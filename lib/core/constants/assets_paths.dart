b35a704d9c83e98b69689bb1e372beb6cdcee563
Kasun Dulara <kasundularaam@gmail.com>
2022-11-19 01:16:48 +0530

    code cleaned

--- lib/core/constants/assets_paths.dart
+++ lib/core/constants/assets_paths.dart
@@ -1,6 +1,4 @@
 class AssetsPaths {
   static const String images = "assets/images";
-  static const String landing = "$images/landing.png";
-  static const String user = "$images/user.png";
   static const String logo = "$images/logo.png";
 }
